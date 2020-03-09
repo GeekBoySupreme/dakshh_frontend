8e3f25c2da82d3e4dec2259b10e46b349618f626
Subhasis Das <subhasisdas125@gmail.com>
2020-03-09 16:52:56 +0530

    added email and phone verification frontend

---
+++ assets/js/verification.js
@@ -0,0 +1,9 @@
+const verifyEmail = () => {
+  /**Complete this function for sending verification email via API call */
+  console.log("Api call for email verification...");
+};
+
+const verifyPhone = () => {
+  /**Complete this function for sending OTP via API call */
+  console.log("Api call for phone verification...");
+};
